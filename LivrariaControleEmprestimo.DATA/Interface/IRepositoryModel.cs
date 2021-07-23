4fe56aa2bff6afaea7b280cf1bc96304b272dfa1
mauriciooliveiratech <mauricioazol@gmail.com>
2021-07-23 17:38:10 -0300

    Inicio do Sistema, configuração do repository e Interface

---
+++ LivrariaControleEmprestimo.DATA/Interface/IRepositoryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivrariaControleEmprestimo.DATA.Interface
+{
+    interface IRepositoryModel<T> where T : class
+    {
+        List<T> SelecionarTodos();
+        T SelecionarPK(params object[] variavel);
+        T Incluir(T objeto);
+        T Alterar(T objeto);
+        void Excluir(T objeto);
+        void Excluir(params object[] variavel);
+        void SaveChages();
+    }
+}
